d3b05bfb6e158fd6c895bf6064d6683c04357fc0
James Daugherty <jamesdaugherty@protonmail.com>
2021-03-08 21:14:23 -0500

    Created comments for JS file & updated README

---
+++ assets/javascript/script.js
@@ -0,0 +1,55 @@
+// Assignment Code
+/* GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+
+WHEN prompted for character types to include in the password
+THEN I choose lowercase, uppercase, numeric, and/or special characters
+
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page 
+*/ 
+
+//Create variables 
+
+
+
+//Create a function to determine password length
+
+
+
+//Create a function used to determine whether the user wants to include uppercase characters
+
+
+
+//Create a function used to determine whether the user wants to include numbers in the password
+
+
+
+//Create a function used to determine whether the user wants to include special characters in the password
+
+
+
+ /*Create a function used to take all the input from the previous functions and generate a password using a random number generator and 
+ the charAt method 
+ */
+
+
+
+// Write password to the #password input
+
+
+
+// Add event listener to generate button
